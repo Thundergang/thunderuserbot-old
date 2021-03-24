0826766ddc71e0605f8fbd676e6b5fb0c59f913a
DeadAnonymous <65858180+deadanonymous@users.noreply.github.com>
2021-03-24 11:02:34 +0530

    Update __init__.py

--- thunderbot/plugins/sql_helper/__init__.py
+++ thunderbot/plugins/sql_helper/__init__.py
@@ -3,3 +3,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 from thunderbot.thunderbotConfig import Var
+
+
+
+def start() -> scoped_session:
+    engine = create_engine(Var.DB_URI)
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+try:
+    BASE = declarative_base()
+    SESSION = start()
