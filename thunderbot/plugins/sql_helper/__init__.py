4ad0f6b3b8e08751e2d36f26bc58b56647105da0
DeadAnonymous <65858180+deadanonymous@users.noreply.github.com>
2021-03-24 10:55:17 +0530

    Update __init__.py

--- thunderbot/plugins/sql_helper/__init__.py
+++ thunderbot/plugins/sql_helper/__init__.py
@@ -2,6 +2,4 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
-
-
 from thunderbot.thunderbotConfig import Var
