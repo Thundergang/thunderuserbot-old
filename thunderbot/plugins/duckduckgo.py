4cd03bfbdcfd278b0a62347bb5fbca1f160c9e2f
DeadAnonymous <65858180+deadanonymous@users.noreply.github.com>
2021-04-19 11:10:10 +0530

    Create duckduckgo.py
    Adding old plugins

---
+++ thunderbot/plugins/duckduckgo.py
@@ -0,0 +1,25 @@
+from telethon import events
+import os
+import requests
+import json
+from thunderbot.utils import admin_cmd
+from thunderbot import CMD_HELP
+
+@thunderbot.on(admin_cmd("duckduckgo (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    sample_url = "https://duckduckgo.com/?q={}".format(input_str.replace(" ","+"))
+    if sample_url:
+        link = sample_url.rstrip()
+        await event.edit("Let Me 🦆 DuckDuckGo That For From My Thunderuserbot⚡️:\n🔎 [{}]({})".format(input_str, link))
+    else:
+        await event.edit("something is wrong. please try again later.")
+
+
+CMD_HELP.update(
+    {
+        "duckduckgo": ".duckduckgo\nUse - To Get Direct Search Link To Duckduckgo."
+    }
+)
