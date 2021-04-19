57f1c5e5669f5a9881960cb78a97ce045beb0dbc
DeadAnonymous <65858180+deadanonymous@users.noreply.github.com>
2021-04-19 11:25:57 +0530

    Create dump.py
    Adding old plugins

---
+++ thunderbot/plugins/dump.py
@@ -0,0 +1,36 @@
+from telethon import events
+import asyncio
+from thunderbot.utils import admin_cmd
+from thunderbot import CMD_HELP
+
+@thunderbot.on(admin_cmd(pattern="dump ?(.*)"))
+async def _(message):
+    try:
+        obj = message.pattern_match.group(1)
+        if len(obj) != 3:
+            raise IndexError
+        inp = ' '.join(obj)
+    except IndexError:
+        inp = "🌎 📱 ❤️"
+    u, t, g, o, s, n = inp.split(), '🗑', '<(^_^ <)', '(> ^_^)>', '⠀ ', '\n'
+    h = [(u[0], u[1], u[2]), (u[0], u[1], ''), (u[0], '', '')]
+    for something in reversed([y for y in ([''.join(x) for x in (
+    f + (s, g, s + s * f.count(''), t), f + (g, s * 2 + s * f.count(''), t),
+    f[:i] + (o, f[i], s * 2 + s * f.count(''), t), f[:i] + (s + s * f.count(''), o, f[i], s, t),
+    f[:i] + (s * 2 + s * f.count(''), o, f[i], t), f[:i] + (s * 3 + s * f.count(''), o, t),
+    f[:i] + (s * 3 + s * f.count(''), g, t))] for i, f in enumerate(reversed(h)))]):
+        for something_else in something:
+            await asyncio.sleep(0.3)
+            try:
+                await message.edit(something_else)
+            except errors.MessageIdInvalidError:
+                return
+
+
+CMD_HELP.update(
+    {
+        "dump": "**Dump**\
+\n\n**Syntax : **`.dump`\
+\n**Usage :** Dumps The World."
+    }
+)
