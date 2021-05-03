a7fe7032663126b1aeb99fab6aaf68a442d9d37e
DeadAnonymous <65858180+deadanonymous@users.noreply.github.com>
2021-05-03 14:23:59 +0530

    Normal

--- thunderbot/plugins/_help.py
+++ thunderbot/plugins/_help.py
@@ -60,7 +60,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`ThunderUserbot Helper of {DEFAULTUSER} Checkout `**[DOCS](https://docs.thunderuserbot.cf/) For Thunderuserbot**\n\n"""
+            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[Thunderuserbot GitBook](https://thundergang.gitbook.io/thunderuserbot/)**\n\n"""
             try:
                 results = await bot.inline_query(  # pylint:disable=E0602
                     tgbotusername, help_string
