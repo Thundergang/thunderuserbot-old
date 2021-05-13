ae2bff5e4bb5eaa8ed2e0d417db2775a63e7f4d5
DeadAnonymous <65858180+deadanonymous@users.noreply.github.com>
2021-05-13 19:11:02 +0530

    #new
    Usage .mmf

---
+++ thunderbot/plugins/mmf.py
@@ -0,0 +1,142 @@
+import os
+import textwrap
+from PIL import Image, ImageDraw, ImageFont
+from thunderbot import CMD_HELP
+from thunderbot.utils import admin_cmd
+
+
+@thunderbot.on(admin_cmd(pattern="mmf ?(.*)"))
+async def handler(event):
+    if event.fwd_from:
+        return
+    if not event.reply_to_msg_id:
+        await event.reply("Format:- memify upper text ; lower text")
+        return
+    reply_message = await event.get_reply_message()
+    if not reply_message.media:
+        await eor(event, "Reply To A Image Or Sticker.")
+        return
+    file = await borg.download_media(reply_message, Var.TEMP_DOWNLOAD_DIRECTORY)
+    a = await event.reply("Processing... ⚡️")
+    text = str(event.pattern_match.group(1)).strip()
+    if len(text) < 1:
+        return await a.edit("Format:- memify upper text ; lower text")
+    meme = await drawText(file, text)
+    await event.client.send_file(event.chat_id, file=meme, force_document=False)
+    os.remove(meme)
+    await event.delete()
+    await a.delete()
+
+
+async def drawText(image_path, text):
+    img = Image.open(image_path)
+    os.remove(image_path)
+    i_width, i_height = img.size
+    if os.name == "nt":
+        fnt = "thundergangfont.ttf"
+    else:
+        fnt = "Extras/thundergangfont.ttf"
+    m_font = ImageFont.truetype(fnt, int((70 / 640) * i_width))
+    if ";" in text:
+        upper_text, lower_text = text.split(";")
+    else:
+        upper_text = text
+        lower_text = ""
+    draw = ImageDraw.Draw(img)
+    current_h, pad = 10, 5
+    if upper_text:
+        for u_text in textwrap.wrap(upper_text, width=15):
+            u_width, u_height = draw.textsize(u_text, font=m_font)
+            draw.text(
+                xy=(((i_width - u_width) / 2) - 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2) + 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=((i_width - u_width) / 2, int(((current_h / 640) * i_width)) - 2),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(((i_width - u_width) / 2), int(((current_h / 640) * i_width)) + 2),
+                text=u_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=((i_width - u_width) / 2, int((current_h / 640) * i_width)),
+                text=u_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+    if lower_text:
+        for l_text in textwrap.wrap(lower_text, width=15):
+            u_width, u_height = draw.textsize(l_text, font=m_font)
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) - 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    ((i_width - u_width) / 2) + 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) - 2,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    (i_height - u_height - int((20 / 640) * i_width)) + 2,
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(0, 0, 0),
+            )
+
+            draw.text(
+                xy=(
+                    (i_width - u_width) / 2,
+                    i_height - u_height - int((20 / 640) * i_width),
+                ),
+                text=l_text,
+                font=m_font,
+                fill=(255, 255, 255),
+            )
+            current_h += u_height + pad
+    image_name = "memify.webp"
+    webp_file = os.path.join(Var.TEMP_DOWNLOAD_DIRECTORY, image_name)
+    img.save(webp_file, "webp")
+    return webp_file
+
+
+CMD_HELP.update(
+    {
+        "memeify": ".mmf <text on top> ; <text on bottom> (reply to image/sticker)\nUse - Memeifies  image/sticker."
+    }
+)
