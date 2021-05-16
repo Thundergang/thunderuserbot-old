2bd7b750baa71a2385521f0871fa801137dbc3e3
NandyDark <58636468+nandydark@users.noreply.github.com>
2021-05-16 19:46:13 +0530

    Update restart.py

--- thunderbot/plugins/restart.py
+++ thunderbot/plugins/restart.py
@@ -1,29 +1,22 @@
-"""
-Available Commands:
-.restart
-"""
-
-import asyncio
-from telethon import events
-from thunderbot import CMD_HELP
+import os
+import sys
+from thunderbot import CMD_HELP, CMD_HNDLR
 from thunderbot.utils import admin_cmd
 
-@thunderbot.on(admin_cmd("restart"))
+
+@thunderbot.on(admin_cmd(pattern="restart"))
 async def _(event):
     if event.fwd_from:
         return
-    animation_interval = 0.1
-    animation_ttl = range(0,2)
-    await event.edit("⚡️Thunderuserbot⚡️")
-    animation_chars = [
-            "⚡️@thunderuserbot⚡️",
-            "⚡️**Restart Your Dynos Manually From Heroku**⚡️\nFor More, Get Help From [Here](https://t.me/thunderuserbot)"
-         ]
-            
+    await event.edit(
+        f"**Restarting Your Thunderuserbot**.. Please Wait Until It Starts Again "
+    )
+    await thunderbot.disconnect()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
 
-    for i in animation_ttl:
-        	
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 18])
-
-CMD_HELP.update({"restart": ".restart\nUse - Restartttttt."})
+CMD_HELP.update(
+    {
+        "restart": ".restart\nUse - Restarts the bot."
+    }
+)
