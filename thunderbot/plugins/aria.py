4e3d6ee341f505d7608e8d356db2db2c17779c17
NandyDark <58636468+nandydark@users.noreply.github.com>
2021-05-23 16:19:08 +0530

    Update aria.py

--- thunderbot/plugins/aria.py
+++ thunderbot/plugins/aria.py
@@ -60,10 +60,10 @@ if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
     os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
 download_path = os.getcwd() + TEMP_DOWNLOAD_DIRECTORY.strip(".")
 
-#aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=8210,
-                                 #secret=""))
+aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=8210,
+                                 secret=""))
 
-#aria2.set_global_options({"dir": download_path})
+aria2.set_global_options({"dir": download_path})
 
 
 #@register(outgoing=True, pattern=r"^\.amag(?: |$)(.*)")
