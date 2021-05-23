9fa0fecc875d8254db8d0ec34c0a6e619e88a24e
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-05-23 15:10:22 +0400

    Update aria.py

--- thunderbot/plugins/aria.py
+++ thunderbot/plugins/aria.py
@@ -62,7 +62,7 @@ download_path = os.getcwd() + TEMP_DOWNLOAD_DIRECTORY.strip(".")
 aria2 = aria2p.API(aria2p.Client(host="http://localhost", port=8210,
                                  secret=""))
 
-aria2.set_global_options({"dir": download_path})
+#aria2.set_global_options({"dir": download_path})
 
 
 #@register(outgoing=True, pattern=r"^\.amag(?: |$)(.*)")
