ea3492b15347d468c1b8f72a11e10abb2805000f
DeadAnonymous <65858180+deadanonymous@users.noreply.github.com>
2021-05-23 17:55:00 +0530

    Create clock.py

---
+++ thunderbot/plugins/clock.py
@@ -0,0 +1,21 @@
+from telethon import events
+import asyncio
+from collections import deque
+from thunderbot.utils import admin_cmd
+from thunderbot import CMD_HELP
+
+@thunderbot.on(admin_cmd(pattern="clock"))
+async def _(event):
+	if event.fwd_from:
+		return
+	deq = deque(list("🕛🕐🕑🕒🕓🕔🕕🕖🕗🕘🕙🕚"))
+	for _ in range(60):
+		await asyncio.sleep(0.1)
+		await event.edit("".join(deq))
+		deq.rotate(1)
+
+CMD_HELP.update(
+    {
+        "clock": "➟ `.clock \nUse - Clock Clock Clock"
+    }
+)
