b2fca964a506b14a8d366ee73a8f42f7e24dd866
DeadAnonymous <65858180+deadanonymous@users.noreply.github.com>
2021-05-30 14:44:31 +0530

    Update support.py

--- thunderbot/plugins/support.py
+++ thunderbot/plugins/support.py
@@ -1,30 +1,19 @@
-"""Emoji
+"""
 Available Commands:
 .support
 """
 
 from telethon import events
-
 import asyncio
-
 from thunderbot.utils import admin_cmd
 
 @thunderbot.on(admin_cmd("support"))
-async def _(event):
+async def support(event):
     if event.fwd_from:
         return
-    animation_interval = 0.1
-    animation_ttl = range(0,36)
-    #input_str = event.pattern_match.group(1)
-   # if input_str == "support":
-    await event.edit("for our support group")
-    animation_chars = [
-            "Click here",
-            "[ThunderUserbot Support](https://t.me/thunderuserbot)"
-         ]
-            
-
-    for i in animation_ttl:
-        	
-        await asyncio.sleep(animation_interval)
-        await event.edit(animation_chars[i % 18])
+    await event.edit(
+        "⚡**Thunderuserbot**⚡\n"
+        "•[Channel](https://t.me/thunderuserbot)\n"
+        "•[Support Group](https://t.me/thunderuserbotchat)\n"
+        "•[Offtopic](https://t.me/thunderuserbotspam)\n"
+)
